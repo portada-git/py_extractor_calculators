49d457b736221dc860571bb9cb5b66dea79c8930
jcanell4 <jcanell4@xtec.cat>
2025-05-29 12:43:28 +0200

    version 0.0.4

--- py_extractor_calculators/__init__.py
+++ py_extractor_calculators/__init__.py
@@ -1,1 +1,2 @@
 from .sm.sm_calculators import get_arrival_date_from_publication_date, get_departure_date, get_duration_value, get_quarantine, get_port_of_call_list
+from .sm import sm_calculators
---
+++ py_extractor_calculators/sm/__init__.py
@@ -0,0 +1,1 @@
+from .sm_calculators import get_arrival_date_from_publication_date, get_departure_date, get_duration_value, get_quarantine, get_port_of_call_list
