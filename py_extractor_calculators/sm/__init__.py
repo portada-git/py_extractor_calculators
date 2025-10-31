17309aa353f6a47613a9f89e8756f1f4afa4c1ff
jcanell4 <jcanell4@xtec.cat>
2025-10-31 12:21:08 +0100

    version 0.0.6

--- py_extractor_calculators/sm/__init__.py
+++ py_extractor_calculators/sm/__init__.py
@@ -1,1 +1,1 @@
-from .sm_calculators import get_arrival_date_from_publication_date, get_departure_date, get_duration_value, get_quarantine, get_port_of_call_list
+from .sm_calculators import get_arrival_date_from_publication_date, get_departure_date, get_duration_value, get_quarantine, get_port_of_call_list, add_consignee_to_cargo_list
